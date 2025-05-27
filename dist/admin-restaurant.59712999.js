451b93b6a5bad9bb50e8eb7aa2772837cfc9acc0
Nathalie Fredriksson <nfredriksson1@gmail.com>
2025-05-27 17:31:05 +0200

    lade in checkauth på alla sidor förutom login

--- dist/admin-restaurant.59712999.js
+++ dist/admin-restaurant.59712999.js
@@ -160,11 +160,11 @@
       });
     }
   }
-})({"71PJ8":[function(require,module,exports,__globalThis) {
+})({"9JJ5D":[function(require,module,exports,__globalThis) {
 var global = arguments[3];
 var HMR_HOST = null;
 var HMR_PORT = null;
-var HMR_SERVER_PORT = 52740;
+var HMR_SERVER_PORT = 1234;
 var HMR_SECURE = false;
 var HMR_ENV_HASH = "d6ea1d42532a7575";
 var HMR_USE_SSE = false;
@@ -668,6 +668,6 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 
 },{}],"4M6V8":[function(require,module,exports,__globalThis) {
 
-},{}]},["71PJ8","4M6V8"], "4M6V8", "parcelRequire3287", {})
+},{}]},["9JJ5D","4M6V8"], "4M6V8", "parcelRequire3287", {})
 
 //# sourceMappingURL=admin-restaurant.59712999.js.map
